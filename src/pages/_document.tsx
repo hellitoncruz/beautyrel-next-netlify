fb77fec04ce356e6122bfd81d64783730e101d9b
Helliton-Xavier <hellitoncruz@qualitydigital.global>
2024-07-24 15:34:24 -0300

    uploading project with no components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from "next/document";
-
+import { Html, Head, Main, NextScript } from 'next/document'
+ 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+      <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="//fonts.googleapis.com/css?family=DM+Sans:400,400i,500,500i,700,700i&display=swap"
+        />
+        <link rel="stylesheet" href="css/bootstrap.css" />
+        <link rel="stylesheet" href="css/fonts.css" />
+        <link rel="stylesheet" href="css/style.css" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
